0ce73d9f1d397acf6ab76ca281a0c1c2a7fafd36
KartikAroraOfficial <90651659+KartikAroraOfficial@users.noreply.github.com>
2022-06-03 15:59:19 +0530

    Add files via upload

---
+++ bluetooth controlled car.ino
@@ -0,0 +1,44 @@
+char key;
+
+void setup() {
+Serial.begin(9600);
+pinMode(13,OUTPUT);   //left motors forward
+pinMode(12,OUTPUT);   //left motors reverse
+pinMode(11,OUTPUT);   //right motors forward
+pinMode(10,OUTPUT);   //right motors reverse
+
+
+}
+
+void loop() {
+if(Serial.available()){
+  key = Serial.read();
+  Serial.println(key);
+}
+
+if(key == 'F'){            //move forward(all motors rotate in forward direction)
+  digitalWrite(13,HIGH);
+  digitalWrite(11,HIGH);
+}
+
+else if(key == 'B'){      //move reverse (all motors rotate in reverse direction)
+  digitalWrite(12,HIGH);
+  digitalWrite(10,HIGH);
+}
+
+else if(key == 'L'){      //turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
+  digitalWrite(11,HIGH);
+}
+
+else if(key == 'R'){      //turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
+  digitalWrite(13,HIGH);
+}
+
+else if(key == 'S'){      //STOP (all motors stop)
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+}
+delay(100);
+}
